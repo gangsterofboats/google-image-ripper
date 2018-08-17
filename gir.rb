7c18496ab203aa76c0aa0ec561b4b4cece1a83de
gangsterofboats <36470882+gangsterofboats@users.noreply.github.com>
2018-08-17 13:13:39 -0700

    Add bits to uniquify lines

--- gir.rb
+++ gir.rb
@@ -1,19 +1,20 @@
-#!/usr/bin/ruby
-# Google Image Ripper - compile links from Google image searches into an HTML file
-# Copyright (C) 2018 Michael Wiseman 
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#!/usr/bin/env ruby
+####################################################################################
+# Google Image Ripper - compile links from Google image searches into an HTML file #
+# Copyright (C) 2018 Michael Wiseman                                               #
+#                                                                                  #
+# This program is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU General Public License as published by the Free Software    #
+# Foundation, either version 3 of the License, or (at your option) any later       #
+# version.                                                                         #
+#                                                                                  #
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY  #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  #
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.        #
+#                                                                                  #
+# You should have received a copy of the GNU General Public License along with     #
+# this program.  If not, see <https://www.gnu.org/licenses/>.                      #
+####################################################################################
 
 require 'net/http'
 require 'uri'
@@ -23,7 +24,7 @@ fnam = ARGV.join('-')
 srch = ARGV.join('+')
 fnam += '.html'
 
-fh = open(fnam, 'w')
+fh = File.open(fnam, 'w+')
 fh.write('<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href="https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>')
 fh.write("\n<br>\n")
 
@@ -39,5 +40,10 @@ fh.write("\n<br>\n")
     fh.write("#{item}\n")
   end
 end
-
 fh.close
+
+fo = File.readlines(fnam)
+fo.uniq!
+File.open(fnam, 'w+') do |fi|
+  fo.each { |item| fi.write("#{item}") }
+end
