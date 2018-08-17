7f6ccdd5e248071d7361ec94a7036ad827dcb9fc
gangsterofboats <36470882+gangsterofboats@users.noreply.github.com>
2018-08-17 16:27:09 -0700

    Uniquify output

--- gir.py
+++ gir.py
@@ -1,9 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+####################################################################################
+# Google Image Ripper - compile links from Google image searches into an HTML file #
+# Copyright (C) 2018 Michael Wiseman                                               #
+#                                                                                  #
+# This program is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU General Public License as published by the Free Software    #
+# Foundation, either version 3 of the License, or (at your option) any later       #
+# version.                                                                         #
+#                                                                                  #
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY  #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  #
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.        #
+#                                                                                  #
+# You should have received a copy of the GNU General Public License along with     #
+# this program.  If not, see <https://www.gnu.org/licenses/>.                      #
+####################################################################################
+
 import sys
 import re
 import requests
 
-print ' '.join(sys.argv[1:])
+print(' '.join(sys.argv[1:]))
 fnam = '-'.join(sys.argv[1:])
 srch = '+'.join(sys.argv[1:])
 fnam = fnam + '.html'
@@ -11,7 +28,7 @@ fh = open(fnam, 'w')
 fh.write('<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href="https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>')
 fh.write('\n<br>\n')
 for i in range(0, 420, 20):
-    url = 'https://www.google.com/search?q={}&safe=off&tbm=isch&ijn=0&start={}'.format(srch, i)
+    url = f'https://www.google.com/search?q={srch}&safe=off&tbm=isch&ijn=0&start={i}'
     content = requests.get(url).text
     content = re.sub('<', '\n<', content)
     contlines = content.split('\n')
@@ -19,6 +36,12 @@ for i in range(0, 420, 20):
     for s in contlines:
         s = re.sub('.*imgurl=', '<img src="', s)
         s = re.sub('&amp.*', '">', s)
-        fh.write(s)
-        fh.write('\n')
+        fh.write(f'{s}\n')
 fh.close()
+
+f = open(fnam, 'r').read().split('\n')
+fo = list(dict.fromkeys(f))
+ofh = open(fnam, 'w')
+for item in fo:
+    ofh.write(f'{item}\n')
+ofh.close()
