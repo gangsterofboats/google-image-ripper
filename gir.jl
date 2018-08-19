ed75cc4448dd1d33b13ed879c81fa5af24bbdbad
gangsterofboats <36470882+gangsterofboats@users.noreply.github.com>
2018-08-18 22:58:59 -0700

    Update gir.jl

--- gir.jl
+++ gir.jl
@@ -1,5 +1,22 @@
 #!/usr/bin/env julia
-using Requests
+####################################################################################
+# Google Image Ripper - compile links from Google image searches into an HTML file #
+# Copyright (C) 2018 Michael Wiseman                                               #
+#                                                                                  #
+# This program is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU General Public License as published by the Free Software    #
+# Foundation, either version 3 of the License, or (at your option) any later       #
+# version.                                                                         #
+#                                                                                  #
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY  #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  #
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.        #
+#                                                                                  #
+# You should have received a copy of the GNU General Public License along with     #
+# this program.  If not, see <https://www.gnu.org/licenses/>.                      #
+####################################################################################
+
+using HTTP
 
 println(join(ARGS, " "))
 fnam = join(ARGS, "-")
@@ -10,18 +27,27 @@ open(fnam, "w") do fh
     write(fh, "<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href=\"https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch\">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>")
     write(fh, "\n<br>\n")
 
-    for i in 0:20:400
+    for i in 0:20:100
         uri = "https://www.google.com/search?q=$srch&safe=off&tbm=isch&ijn=0&start=$i"
-        cnt = get(uri)
-        content = String(map((c) -> Char(c), cnt.data))
+        r = HTTP.request("GET", uri)
+        content = String(r.body)
         content = replace(content, "<", "\n<")
         contlines = split(content, "\n")
-        cntlines = filter((l) -> contains(l, "imgurl"), contlines)
-        for l in cntlines
+        contlines = filter((l) -> contains(l, r"imgurl"), contlines)
+        for l in contlines
             l = replace(l, r".*imgurl=", "<img src=\"")
             l = replace(l, r"&amp.*", "\">")
             write(fh, l)
             write(fh, "\n")
         end
+    end
+end
+
+fo = readlines(fnam)
+fo = unique(fo)
+open(fnam, "w") do ofh
+    for ln in fo
+        write(ofh, ln)
+        write(ofh, "\n")
     end
 end
