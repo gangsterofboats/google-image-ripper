67b7a38b254cc10283c7d1806e392d0c99652f0c
gangsterofboats <36470882+gangsterofboats@users.noreply.github.com>
2018-08-18 21:23:53 -0700

    Update gir.hs

--- gir.hs
+++ gir.hs
@@ -1,10 +1,31 @@
+{-
+Google Image Ripper - compile links from Google image searches into an HTML file
+Copyright (C) 2018 Michael Wiseman
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
+-}
+
+import Control.Lens
 import Control.Monad
-import Data.Either.Unwrap
 import Data.List
-import Network.Download
+import Data.Typeable
+import Network.Wreq
 import System.Environment
 import System.IO
 import Text.Regex
+
+import qualified Data.ByteString.Lazy.Char8 as C
+import qualified System.IO.Strict as S
 
 main = do
   args <- getArgs
@@ -13,18 +34,23 @@ main = do
   let srch = intercalate "+" args
   fnam <- return $ fnam ++ ".html"
 
-  fh <- openFile fnam WriteMode
-  hPutStrLn fh "<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href=\"https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch\">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>"
-  hPutStrLn fh "<br>"
-  forM_ [0,20..400] $ \i -> do
-    let url = "http://www.google.com/search?q=" ++ srch ++ "&safe=off&tbm=isch&ijn=0&start=" ++ show i
-    resp <- openURIString url
-    let content = fromRight resp
-    content <- return $ subRegex (mkRegex "<") content "\n<"
+  writeFile fnam "<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href=\"https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch\">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>"
+  appendFile fnam "\n<br>\n"
+  forM_ [0,20..100] $ \i -> do
+    let uri = "https://www.google.com/search?q=" ++ srch ++ "&safe=off&tbm=isch&ijn=0&start=" ++ show i
+    resp <- get uri
+    let content = resp ^. responseBody
+    content <- return $ subRegex (mkRegex "<") (C.unpack content) "\n<"
     let contlines = splitRegex (mkRegex "\n") content
     contlines <- return $ filter (isInfixOf "imgurl") contlines
     forM_ contlines $ \s -> do
       s <- return $ subRegex (mkRegex ".*imgurl=") s "<img src=\""
       s <- return $ subRegex (mkRegex "&amp.*") s "\">"
-      hPutStrLn fh s
-  hClose fh
+      appendFile fnam s
+      appendFile fnam "\n"
+
+  fs <- S.readFile fnam
+  let fo = splitRegex (mkRegex "\n") fs
+  fo <- return $ nub fo
+  let fs = intercalate "\n" fo
+  writeFile fnam fs
