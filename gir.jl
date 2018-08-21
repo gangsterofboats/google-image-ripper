bdada0a94f5f2847f848f2d96fb12b221aad69ff
gangsterofboats <36470882+gangsterofboats@users.noreply.github.com>
2018-08-20 22:01:18 -0700

    Update for Julia 1.0

--- gir.jl
+++ gir.jl
@@ -19,26 +19,24 @@
 using HTTP
 
 println(join(ARGS, " "))
-fnam = join(ARGS, "-")
+fnam = join(ARGS, "-") * ".html"
 srch = join(ARGS, "+")
-fnam = fnam * ".html"
 
 open(fnam, "w") do fh
     write(fh, "<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href=\"https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch\">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>")
     write(fh, "\n<br>\n")
 
-    for i in 0:20:100
+    for i in 0:20:400
         uri = "https://www.google.com/search?q=$srch&safe=off&tbm=isch&ijn=0&start=$i"
         r = HTTP.request("GET", uri)
         content = String(r.body)
-        content = replace(content, "<", "\n<")
+        content = replace(content, "<" => "\n<")
         contlines = split(content, "\n")
-        contlines = filter((l) -> contains(l, r"imgurl"), contlines)
+        contlines = filter((l) -> occursin("imgurl", l), contlines)
         for l in contlines
-            l = replace(l, r".*imgurl=", "<img src=\"")
-            l = replace(l, r"&amp.*", "\">")
-            write(fh, l)
-            write(fh, "\n")
+            l = replace(l, r".*imgurl=" => "<img src=\"")
+            l = replace(l, r"&amp.*" => "\">")
+            write(fh, "$l\n")
         end
     end
 end
@@ -47,7 +45,6 @@ fo = readlines(fnam)
 fo = unique(fo)
 open(fnam, "w") do ofh
     for ln in fo
-        write(ofh, ln)
-        write(ofh, "\n")
+        write(ofh, "$ln\n")
     end
 end
