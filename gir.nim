0452e9baa72be749cc636c26600f3fdecb20aadd
gangsterofboats <36470882+gangsterofboats@users.noreply.github.com>
2018-08-21 21:16:33 -0700

    Update gir.nim

--- gir.nim
+++ gir.nim
@@ -1,27 +1,27 @@
 #!/usr/bin/env nim
-# Google Image Ripper - compile links from Google image searches into an HTML file
-# Copyright (C) 2018 Michael Wiseman
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+####################################################################################
+# Google Image Ripper - compile links from Google image searches into an HTML file #
+# Copyright (C) 2018 Michael Wiseman                                               #
+#                                                                                  #
+# This program is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU General Public License as published by the Free Software    #
+# Foundation, either version 3 of the License, or (at your option) any later       #
+# version.                                                                         #
+#                                                                                  #
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY  #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  #
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.        #
+#                                                                                  #
+# You should have received a copy of the GNU General Public License along with     #
+# this program.  If not, see <https://www.gnu.org/licenses/>.                      #
+####################################################################################
 
 import os, strutils, httpclient, sequtils, re
 
 var args = commandLineParams()
 echo join(args, " ")
-var fileName = join(args, "-")
+var fileName = join(args, "-") & ".html"
 var searchTerm = join(args, "+")
-fileName = fileName & ".html"
 
 var fh = open(fileName, fmWrite)
 fh.writeLine("<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href=\"https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch\">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>")
@@ -39,3 +39,9 @@ for i in countup(0,100,20):
     fh.writeLine(line)
 
 fh.close()
+
+var fs = readFile(fileName)
+var fo = splitLines(fs)
+fo = deduplicate(fo)
+fs = join(fo, "\n")
+writeFile(fileName, fs)
