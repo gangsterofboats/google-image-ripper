259606e31f009848769fdc875a8a7a05870ce46b
gangsterofboats <36470882+gangsterofboats@users.noreply.github.com>
2018-08-24 12:18:35 -0700

    Update gir.r

--- gir.r
+++ gir.r
@@ -1,3 +1,21 @@
+#!/usr/bin/env Rscript
+####################################################################################
+# Google Image Ripper - compile links from Google image searches into an HTML file #
+# Copyright (C) 2018 Michael Wiseman                                               #
+#                                                                                  #
+# This program is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU General Public License as published by the Free Software    #
+# Foundation, either version 3 of the License, or (at your option) any later       #
+# version.                                                                         #
+#                                                                                  #
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY  #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  #
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.        #
+#                                                                                  #
+# You should have received a copy of the GNU General Public License along with     #
+# this program.  If not, see <https://www.gnu.org/licenses/>.                      #
+####################################################################################
+
 argv <- (commandArgs(TRUE))
 args <- paste(argv, collapse=" ")
 print(args)
@@ -5,11 +23,11 @@ fnam <- paste(argv, collapse="-")
 srch <- paste(argv, collapse="+")
 fnam <- paste(fnam, ".html", sep="")
 
-fh <- file(fnam, open="a")
+fh <- file(fnam, open="w")
 writeLines('<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href="https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>', fh)
 writeLines('<br>', fh)
 
-for (i in seq(0, 400, by=20))
+for (i in seq(0, 100, by=20))
 {
     uri <- paste("https://www.google.com/search?q=", srch, "&safe=off&tbm=isch&ijn=0&start=", i, sep="")
     content <- readLines(uri)
@@ -18,6 +36,10 @@ for (i in seq(0, 400, by=20))
     contlines <- contlines[grep("imgurl", contlines)]
     contlines <- gsub(".*imgurl=", '<img src="', contlines)
     contlines <- gsub("&amp.*", '">', contlines)
-    write(contlines, fh, append=TRUE)
+    writeLines(contlines, fh)
 }
 close(fh)
+
+fo <- readLines(fnam)
+fo <- unique(fo)
+writeLines(fo, fnam)
