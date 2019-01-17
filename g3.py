d2f4d031b3fb46591c7d42c29f3dbeba629cec16
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2019-01-16 21:15:57 -0800

    Update g3.py

--- g3.py
+++ g3.py
@@ -15,10 +15,10 @@
 # You should have received a copy of the GNU General Public License along with     #
 # this program.  If not, see <https://www.gnu.org/licenses/>.                      #
 ####################################################################################
-
-import sys
+import json
 import re
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 print(' '.join(sys.argv[1:]))
@@ -29,21 +29,21 @@ fh = open(file_name, 'w')
 fh.write('<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href="https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>')
 fh.write('\n<br>\n')
 
-for i in range(0, 120, 20):
+for i in range(0, 420, 20):
     uri = f'https://www.google.com/search?q={search}&safe=off&tbm=isch&ijn=0&start={i}'
-    doc = requests.get(uri).text
+    headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+    doc = requests.get(uri, headers=headers).text
     content = BeautifulSoup(doc, 'html.parser')
-    images = content.select('.rg_l')
-    for line in images:
-        img = line['href']
-        img = re.sub('.*imgurl=', '<img src="', img)
-        img = re.sub('&imgrefurl.*', '">', img)
-        fh.write(f'{img}\n')
+    images = content.select('.rg_meta')
+    for img in images:
+        nosj = json.loads(img.contents[0])
+        egami = nosj['ou']
+        fh.write(f'<img src="{egami}">\n')
 fh.close()
 
-# f = open(file_name, 'r').read().split('\n')
-# fo = list(dict.fromkeys(f))
-# ofh = open(file_name, 'w')
-# for item in fo:
-    # ofh.write(f'{item}\n')
-# ofh.close()
+f = open(file_name, 'r').read().split('\n')
+fo = list(dict.fromkeys(f))
+ofh = open(file_name, 'w')
+for item in fo:
+    ofh.write(f'{item}\n')
+ofh.close()
