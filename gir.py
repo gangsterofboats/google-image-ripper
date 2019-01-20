48d7fb2d1fbc08555517dd7bdd312a7942de81c2
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2019-01-19 19:54:17 -0800

    Update gir.py

--- gir.py
+++ gir.py
@@ -15,27 +15,28 @@
 # You should have received a copy of the GNU General Public License along with     #
 # this program.  If not, see <https://www.gnu.org/licenses/>.                      #
 ####################################################################################
-
-import sys
 import re
 import requests
+import sys
 
 print(' '.join(sys.argv[1:]))
-fnam = '-'.join(sys.argv[1:])
+fnam = '-'.join(sys.argv[1:]) + '.html'
 srch = '+'.join(sys.argv[1:])
-fnam = fnam + '.html'
+
 fh = open(fnam, 'w')
 fh.write('<h1>NOTICE: RMCCURDY.COM IS NOT RESPONSIBLE FOR ANY CONTENT ON THIS PAGE. THIS PAGE IS A RESULT OF IMAGES.GOOGLE.COM INDEXING AND NO CONTENT IS HOSTED ON THIS SITE. PLEASE SEND ANY COPYRIGHT NOTICE INFORMATION TO <a href="https://support.google.com/legal/contact/lr_dmca?dmca=images&product=imagesearch">GOOGLE</a> OR THE OFFENDING WEBSITE</h1>')
 fh.write('\n<br>\n')
-for i in range(0, 420, 20):
+
+for i in range(0, 120, 20):
     url = f'https://www.google.com/search?q={srch}&safe=off&tbm=isch&ijn=0&start={i}'
-    content = requests.get(url).text
+    headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+    content = requests.get(url, headers=headers).text
     content = re.sub('<', '\n<', content)
     contlines = content.split('\n')
-    contlines = [x for x in contlines if 'imgurl' in x]
+    contlines = [x for x in contlines if '"ou"' in x]
     for s in contlines:
-        s = re.sub('.*imgurl=', '<img src="', s)
-        s = re.sub('&amp.*', '">', s)
+        s = re.sub('.*ou":', '<img src=', s)
+        s = re.sub(',"ow.*', '>', s)
         fh.write(f'{s}\n')
 fh.close()
 
