4cbedaae0906ba47b4602310edea187963d54fdf
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2020-07-14 15:11:50 -0700

    Update gir.rb

--- gir.rb
+++ gir.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/env ruby
 ####################################################################################
 # Google Image Ripper - compile links from Google image searches into an HTML file #
-# Copyright (C) 2018 Michael Wiseman                                               #
+# Copyright (C) 2020 Michael Wiseman                                               #
 #                                                                                  #
 # This program is free software: you can redistribute it and/or modify it under    #
-# the terms of the GNU General Public License as published by the Free Software    #
-# Foundation, either version 3 of the License, or (at your option) any later       #
-# version.                                                                         #
+# the terms of the GNU Affero General Public License as published by the Free      #
+# Software Foundation, either version 3 of the License, or (at your option) any    #
+# later version.                                                                   #
 #                                                                                  #
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY  #
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  #
-# PARTICULAR PURPOSE.  See the GNU General Public License for more details.        #
+# PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. #
 #                                                                                  #
-# You should have received a copy of the GNU General Public License along with     #
-# this program.  If not, see <https://www.gnu.org/licenses/>.                      #
+# You should have received a copy of the GNU Affero General Public License along   #
+# with this program.  If not, see <https://www.gnu.org/licenses/>.                 #
 ####################################################################################
 require 'http'
 
